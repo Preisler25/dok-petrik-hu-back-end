6bdcba07588e366ba853f3994c542d46d4a59abf
Preisler <preisler.andras@gmail.com>
2023-09-22 08:44:31 +0200

    main

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
 import db from './db_config';
-import cors from 'cors';
 
 dotenv.config();
 
@@ -20,15 +19,9 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
+// section Projects
+
 app.get('/get_projects', async (req: Request, res: Response) => {
-    /*
-    try {
-        const users = await db.any('SELECT * FROM projects');
-        res.json(users);
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'An error occurred' });
-    }*/
     db.any('SELECT id FROM projects')
         .then((data) => {
             res.json(data);
@@ -68,7 +61,9 @@ app.post('/set_new_project', async (req: Request, res: Response) => {
 
 app.delete('/delet_project', async (req: Request, res: Response) => {
     const { id, s_key } = req.body;
-
+    console.log('====================================');
+    console.log(id, s_key);
+    console.log('====================================');
     db.any(`DELETE FROM projects WHERE id = ${id} AND s_key = '${s_key}'`)
         .then((data) => {
             res.json(data);
@@ -78,6 +73,48 @@ app.delete('/delet_project', async (req: Request, res: Response) => {
             res.status(500).json({ error: 'An error occurred' });
         });
 });
+
+//section end
+
+//section tipps
+
+/*type Tipps = {
+    name: string;
+    cls: string;
+    prof_link: string;
+    msg: string;
+};
+
+export default Tipps;*/
+app.post('/set_new_tipp', async (req: Request, res: Response) => {
+    const { name, cls, prof_link, msg } = req.body;
+
+    db.any(`INSERT INTO tipps (name, cls, prof_link, msg) VALUES ('${name}', '${cls}', '${prof_link}', '${msg}')`)
+        .then((data) => {
+            res.json(data);
+        }
+        )
+        .catch((error) => {
+            console.error('Error:', error);
+            res.status(500).json({ error: 'An error occurred' });
+        });
+}
+);
+
+app.get('/get_tipps', async (req: Request, res: Response) => {
+    db.any('SELECT * FROM tipps')
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            res.status(500).json({ error: 'An error occurred' });
+        });
+});
+
+//end section
+
+
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
